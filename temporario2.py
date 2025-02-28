afe2d52bc6a2be09e36dfacfdae065ce11a35f23
Pedro H Godinho <166649036+g0d1nn@users.noreply.github.com>
2025-02-28 20:12:27 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -1,6 +1,6 @@
 import datetime
 import streamlit as st
 
-d = st.date_input("When's your birthday", value=None)
-st.write("Your birthday is:", d)                                         
+d = st.date_input("Data de nascimento", value=None)
+                                        
     
