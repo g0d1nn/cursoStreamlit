af2781a4b792e2638fa3ac37f8581db8d2334a6b
Pedro H Godinho <166649036+g0d1nn@users.noreply.github.com>
2025-02-28 20:14:29 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -1,6 +1,9 @@
 import datetime
 import streamlit as st
 
+title = st.text_input("Escreva seu nome", " ")
+
+
 d = st.date_input("Data de nascimento", value=None)
                                         
     
