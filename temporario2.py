c72fb1301da8974449bf8d610992c4cd20a400b1
Pedro H Godinho <166649036+g0d1nn@users.noreply.github.com>
2025-02-28 20:10:40 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -1,16 +1,5 @@
 import streamlit as st
-import pandas as pd
+import datetime
 
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
-
-with col2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
+d = st.date_input("Data de nascimento", datetime.date(2004, 5, 2"))                                                      
+    
