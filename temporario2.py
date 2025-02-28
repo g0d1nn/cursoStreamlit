7b0c6dccf5d4df02496dda6167736f96bcdf78a4
Pedro H Godinho <166649036+g0d1nn@users.noreply.github.com>
2025-02-28 20:23:32 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -4,11 +4,12 @@ import streamlit as st
 ra = st.number_input(
     "Escreva o seu RA", value=None, placeholder="Escreva um numero..."
 )
-nome = st.text_input("Escreva seu nome", " ", key= "i_1")
 
 cpf = st.number_input(
     "Escreva o seu CPF", value=None, placeholder="Escreva um numero..."
 )
+
+nome = st.text_input("Escreva seu nome", " ", key= "i_1")
 
 
 d = st.date_input("Data de nascimento", value=None)
