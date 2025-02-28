8723811cc2993e54f5163236f1610d12238cc201
Pedro H Godinho <166649036+g0d1nn@users.noreply.github.com>
2025-02-28 20:30:26 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -15,5 +15,13 @@ nome = st.text_input("Escreva seu nome", " ", key= "i_1")
 
 
 d = st.date_input("Data de nascimento", value=None)
+
+uploaded_files = st.file_uploader(
+    "Escolha uma foto", accept_multiple_files=True
+)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
                                         
     
