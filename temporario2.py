c1b4e2e4f6ddee91657e917dbe5fa2cd533a007f
Pedro H Godinho <166649036+g0d1nn@users.noreply.github.com>
2025-02-28 20:21:55 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -2,7 +2,9 @@ import datetime
 import streamlit as st
 
 nome = st.text_input("Escreva seu nome", " ", key= "i_1")
-cpf = st.number_input("Escreva seu cpf", " ", key= "i_2" )
+number = st.number_input(
+    "Insert a number", value=None, placeholder="Type a number..."
+)
 
 
 d = st.date_input("Data de nascimento", value=None)
