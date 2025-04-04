36ea3f87dfc63f81b450f37a44b00152fb735d94
Pedro H Godinho <166649036+g0d1nn@users.noreply.github.com>
2025-04-04 20:24:14 -0300

    Update Prova.py

--- Prova.py
+++ Prova.py
@@ -3,12 +3,25 @@ import streamlit as st
 st.title("Cadastro de funcionario")
 
 nome = st.text_input("Digite o seu nome")
-print(nome)
+
 sobrenome = st.text_input("Digite o seu sobrenome")
-print(sobrenome)
+
 dataNasc = st.date_input("Digite sua data de nascimento")
-print(dataNasc)
+
 estadoCivil = st.text_input("Digite o seu estado civil")
-print(estadoCivil)
+
 salario = st.number_input("Digite o seu salario")
-print(salario)
+
+st.write(f"Nome completo: {nome} {sobrenome}")
+st.write(f"Data de nascimento: {dataNasc}")
+st.write(f"Estado civil: {estadoCivil}")
+st.write(f"Salário: R${salario}")
+
+if salario < 2.500:
+    print("O funcionario deve receber aumento")
+else:
+    print("O funcionario nao deve receber aumento")
+
+aumento = 0
+while aumento < 500:
+    aumento = aumento +100
